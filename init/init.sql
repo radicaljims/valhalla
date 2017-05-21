bec0b30bbdd985990b08939930ac79259a452928
James Sandridge <jims@radical-ideal.com>
2017-05-20 21:05:51 -0400

    Add in a few more tables: status and flux

--- init/init.sql
+++ init/init.sql
@@ -1,5 +1,20 @@
+-- Currently registered valves
 CREATE TABLE valves (
-       id serial PRIMARY KEY,
-       registered timestamp without time zone default (now() at time zone 'utc'),
-       name varchar (50) NOT NULL
+  id serial PRIMARY KEY,
+  registered timestamp without time zone default (now() at time zone 'utc'),
+  label varchar (50) NOT NULL
+);
+
+-- The state of a valve: open or closed
+CREATE TABLE status (
+  is_open boolean NOT NULL,
+  valve integer REFERENCES valves,
+  ts timestamp without time zone default (now() at time zone 'utc')
+);
+
+-- The history of flux (rate of flow) through the valve
+CREATE TABLE flux (
+  quantity real NOT NULL,
+  valve integer REFERENCES valves,
+  ts timestamp without time zone default (now() at time zone 'utc')
 );
